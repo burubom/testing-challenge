8552b3c1166fc7c8640031ecd26d34b7f28f4a5d
burubom <3070270+burubom@users.noreply.github.com>
2024-04-04 20:52:58 +0200

    Add validations to bookstore app

--- Exercise1-API/book-store/api/bookstore_api.py
+++ Exercise1-API/book-store/api/bookstore_api.py
@@ -1,3 +1,5 @@
+import re
+from datetime import datetime
 from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
@@ -17,6 +19,10 @@ def create_book():
     if not all(field in data for field in required_fields):
         return make_response(
             jsonify({'error': 'Missing required fields'}), 400)
+
+    result = validate(data)
+    if result is not None:
+        return result
 
     new_book = {
         'book_id': str(len(books) + 1),
@@ -51,6 +57,11 @@ def update_book(book_id):
         return make_response(jsonify({'error': 'Book not found'}), 404)
 
     data = request.get_json()
+
+    result = validate(data)
+    if result is not None:
+        return result
+
     for field in ['title', 'author', 'published_date', 'isbn', 'price']:
         if field in data:
             book[field] = data[field]
@@ -67,6 +78,31 @@ def delete_book(book_id):
     return make_response(
         jsonify({'message': 'Book deleted successfully'}), 204)
 
+
+def validate(data):
+
+    if 'title' in data and len(data['title']) >= 256:
+        return make_response(jsonify({'error': 'Title has to be maximum 255 characters'}), 400)
+
+    if 'author' in data and len(data['author']) >= 256:
+        return make_response(jsonify({'error': 'Author has to be maximum 255 characters'}), 400)
+
+    if 'published_date' in data:
+        try:
+            datetime.strptime(data['published_date'], '%Y-%m-%d')
+        except ValueError:
+            return make_response(jsonify({'error': 'Invalid Publish Date'}), 400)
+
+    if 'isbn' in data and not re.match(r'^\d{10}$|^\d{13}$', data['isbn']):
+        return make_response(jsonify({'error': 'Invalid ISBN'}), 400)
+
+    if 'price' in data:
+        try:
+            price = float(data['price'])
+            if price <= 0:
+                raise ValueError()
+        except ValueError as e:
+            return make_response(jsonify({'error': 'Invalid Price'}), 400)
 
 if __name__ == '__main__':
     app.run(debug=True)
